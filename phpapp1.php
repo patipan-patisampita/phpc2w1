2796ec9d792538d668f83b0c38a8e136088c0fcb
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-05 11:36:15 +0700

    =initial commit

---
+++ phpapp1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta chaeset="utf8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP: Basic</title>
+    <style>
+        h1{
+            color:red;
+        }
+    </style>
+</head>
+
+<body>
+    <h1 style="color:blueviolet;">
+        <?php echo "PHP Syntax"; ?>
+    </h1>
+</body>
+
+</html>
